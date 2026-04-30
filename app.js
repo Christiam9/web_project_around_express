5a4be9fc9dd6388b75cebc8ba770255081507e34
ChristiamR <christiancho0.9@gmail.com>
2026-04-29 22:05:19 -0500

    modificaciones a app.js y package´s

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cardsRoutes from "./routes/cards.js";
 import usersRoutes from "./routes/users.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use("/cards", cardsRoutes);
 app.use("/users", usersRoutes);
